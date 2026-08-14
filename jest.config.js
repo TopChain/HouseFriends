9cae9962928cc959ec10ad56d97371806e0f18ca
TopChain AI Lab <topchainfresh@gmail.com>
2026-08-13 18:19:38 -0700

    Fix release candidate CI

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   preset: 'jest-expo',
   testMatch: ['<rootDir>/tests/ui/**/*.test.tsx'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFiles: ['<rootDir>/tests/jest.setup.js'],
   clearMocks: true,
   transform: {
     ...expoPreset.transform,
